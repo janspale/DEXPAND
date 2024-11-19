cc2b7d649733e2dd9e1303cb81983b17a08be37a
janspale <jan.spale96@gmail.com>
2024-11-19 11:13:26 +0100

    modified:   design_tools/orc_axial/axial_meanline_design.py

--- design_tools/orc_axial/axial_meanline_design.py
+++ design_tools/orc_axial/axial_meanline_design.py
@@ -22,155 +22,6 @@ from deap import base, creator, tools, algorithms
 from multiprocessing import Pool
 import random
 
-from matplotlib.patches import Arc
-from matplotlib.transforms import Bbox, IdentityTransform, TransformedBbox
-
-class AngleAnnotation(Arc):
-    """
-    Draws an arc between two vectors which appears circular in display space.
-    """
-    def __init__(self, xy, p1, p2, size=75, unit="points", ax=None,
-                 text="", textposition="inside", text_kw=None, **kwargs):
-        """
-        Parameters
-        ----------
-        xy, p1, p2 : tuple or array of two floats
-            Center position and two points. Angle annotation is drawn between
-            the two vectors connecting *p1* and *p2* with *xy*, respectively.
-            Units are data coordinates.
-
-        size : float
-            Diameter of the angle annotation in units specified by *unit*.
-
-        unit : str
-            One of the following strings to specify the unit of *size*:
-
-            * "pixels": pixels
-            * "points": points, use points instead of pixels to not have a
-              dependence on the DPI
-            * "axes width", "axes height": relative units of Axes width, height
-            * "axes min", "axes max": minimum or maximum of relative Axes
-              width, height
-
-        ax : `matplotlib.axes.Axes`
-            The Axes to add the angle annotation to.
-
-        text : str
-            The text to mark the angle with.
-
-        textposition : {"inside", "outside", "edge"}
-            Whether to show the text in- or outside the arc. "edge" can be used
-            for custom positions anchored at the arc's edge.
-
-        text_kw : dict
-            Dictionary of arguments passed to the Annotation.
-
-        **kwargs
-            Further parameters are passed to `matplotlib.patches.Arc`. Use this
-            to specify, color, linewidth etc. of the arc.
-
-        """
-        self.ax = ax or plt.gca()
-        self._xydata = xy  # in data coordinates
-        self.vec1 = p1
-        self.vec2 = p2
-        self.size = size
-        self.unit = unit
-        self.textposition = textposition
-
-        super().__init__(self._xydata, size, size, angle=0.0,
-                         theta1=self.theta1, theta2=self.theta2, **kwargs)
-
-        self.set_transform(IdentityTransform())
-        self.ax.add_patch(self)
-
-        self.kw = dict(ha="center", va="center",
-                       xycoords=IdentityTransform(),
-                       xytext=(0, 0), textcoords="offset points",
-                       annotation_clip=True)
-        self.kw.update(text_kw or {})
-        self.text = ax.annotate(text, xy=self._center, **self.kw)
-
-    def get_size(self):
-        factor = 1.
-        if self.unit == "points":
-            factor = self.ax.figure.dpi / 72.
-        elif self.unit[:4] == "axes":
-            b = TransformedBbox(Bbox.unit(), self.ax.transAxes)
-            dic = {"max": max(b.width, b.height),
-                   "min": min(b.width, b.height),
-                   "width": b.width, "height": b.height}
-            factor = dic[self.unit[5:]]
-        return self.size * factor
-
-    def set_size(self, size):
-        self.size = size
-
-    def get_center_in_pixels(self):
-        """return center in pixels"""
-        return self.ax.transData.transform(self._xydata)
-
-    def set_center(self, xy):
-        """set center in data coordinates"""
-        self._xydata = xy
-
-    def get_theta(self, vec):
-        vec_in_pixels = self.ax.transData.transform(vec) - self._center
-        return np.rad2deg(np.arctan2(vec_in_pixels[1], vec_in_pixels[0]))
-
-    def get_theta1(self):
-        return self.get_theta(self.vec1)
-
-    def get_theta2(self):
-        return self.get_theta(self.vec2)
-
-    def set_theta(self, angle):
-        pass
-
-    # Redefine attributes of the Arc to always give values in pixel space
-    _center = property(get_center_in_pixels, set_center)
-    theta1 = property(get_theta1, set_theta)
-    theta2 = property(get_theta2, set_theta)
-    width = property(get_size, set_size)
-    height = property(get_size, set_size)
-
-    # The following two methods are needed to update the text position.
-    def draw(self, renderer):
-        self.update_text()
-        super().draw(renderer)
-
-    def update_text(self):
-        c = self._center
-        s = self.get_size()
-        angle_span = (self.theta2 - self.theta1) % 360
-        angle = np.deg2rad(self.theta1 + angle_span / 2)
-        r = s / 2
-        if self.textposition == "inside":
-            r = s / np.interp(angle_span, [60, 90, 135, 180],
-                                          [3.3, 3.5, 3.8, 4])
-        self.text.xy = c + r * np.array([np.cos(angle), np.sin(angle)])
-        if self.textposition == "outside":
-            def R90(a, r, w, h):
-                if a < np.arctan(h/2/(r+w/2)):
-                    return np.sqrt((r+w/2)**2 + (np.tan(a)*(r+w/2))**2)
-                else:
-                    c = np.sqrt((w/2)**2+(h/2)**2)
-                    T = np.arcsin(c * np.cos(np.pi/2 - a + np.arcsin(h/2/c))/r)
-                    xy = r * np.array([np.cos(a + T), np.sin(a + T)])
-                    xy += np.array([w/2, h/2])
-                    return np.sqrt(np.sum(xy**2))
-
-            def R(a, r, w, h):
-                aa = (a % (np.pi/4))*((a % (np.pi/2)) <= np.pi/4) + \
-                     (np.pi/4 - (a % (np.pi/4)))*((a % (np.pi/2)) >= np.pi/4)
-                return R90(aa, r, *[w, h][::int(np.sign(np.cos(2*a)))])
-
-            bbox = self.text.get_window_extent()
-            X = R(angle, r, bbox.width, bbox.height)
-            trans = self.ax.figure.dpi_scale_trans.inverted()
-            offs = trans.transform(((X-s/2), 0))[0] * 72
-            self.text.set_position([offs*np.cos(angle), offs*np.sin(angle)])
-
 # Set the REFPROP path
 RP_path = "c:\\Program Files (x86)\\REFPROP"
 
@@ -285,10 +136,7 @@ def meanline_design(D_mid, n_design, alpha_stator, h_over_D_mid, eta_guess, p_in
     no_nozzles = 10 # [-] #CONSTANT!!!!! - to be varied
     b_nozzle_out = b_nozzles_out/no_nozzles # m
     b_nozzle_throat = b_nozzles_throat/no_nozzles # m
-
-     #gammas
-    """
-
+    
     CP.set_config_string(CP.ALTERNATIVE_REFPROP_PATH, os.getenv('RPPREFIX'))
 
     # Define the temperature and entropy ranges for the saturated vapor region
@@ -491,7 +339,7 @@ def meanline_design(D_mid, n_design, alpha_stator, h_over_D_mid, eta_guess, p_in
     plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
     plt.savefig('Gamma_P_v_diagram.png', dpi = 600)
     plt.show()
-    """
+    
 
     #velocity triangles
     c1a = c_nozzle_out*np.sin(np.radians(alpha_stator)) # m/s; axial absolute velocity stator outlet
@@ -511,8 +359,8 @@ def meanline_design(D_mid, n_design, alpha_stator, h_over_D_mid, eta_guess, p_in
     P_guess = m_dot*dh_stage*eta_guess # W; guess for power output
     chords = [0.005,0.0075,0.01,0.0125,0.015,0.2,0.25,0.3,0.4,0.5] #list of chord values
     chord_guess = 1.6*np.sqrt((P_guess/m_dot/1000+25))/1000 # m; guess for rotor chord
-    #chord = min(chords, key=lambda x:abs(x-chord_guess)) #pick the closest chord value from the list
-    chord = 0.0125 # m; rotor chord
+    chord = min(chords, key=lambda x:abs(x-chord_guess)) #pick the closest chord value from the list
+    #chord = 0.0125 # m; rotor chord for GA fixed
     phi_it = 0.957 - 0.000362 * theta - 0.0258 * Ma1r + 0.00000639 * theta**2 + 0.0674 * Ma1r**2 - 0.0000000753 * theta**3 - 0.043 * Ma1r**3 - 0.000238 * theta * Ma1r + 0.00000145 * theta**2 * Ma1r + 0.0000425 * theta * Ma1r**2 # [-]; impulse turbine loss factor, correlation
     def get_phi(phi_it,ht_rotor,chord):
         #need phi_it loop here to get w2 
@@ -578,40 +426,12 @@ def meanline_design(D_mid, n_design, alpha_stator, h_over_D_mid, eta_guess, p_in
 
 def draw_velocity_triangles(c1u,c1a,w1u,w1a,U,c2u,c2a,w2u,w2a,alpha_stator,beta2,alpha_rotor):
     plt.figure()
-    #====stator outlet====
-    #end the line with an arrow
     plt.arrow(0,0,c1u,c1a, head_width=5, head_length=5, fc='red', ec='red', length_includes_head=True)
     plt.arrow(0,0,w1u,w1a, head_width=5, head_length=5, fc='blue', ec='blue', length_includes_head=True)
     plt.arrow(w1u,w1a,U,0, head_width=5, head_length=5, fc='green', ec='green', length_includes_head=True)
-    #add text to the arrows
-   # plt.text(c1u/2+40,w1a/2+5, r'$\overrightarrow{c_2}$', ha='center', va='center', color='red')
-    #plt.text(w1u/2-5,w1a/2+5, r'$\overrightarrow{w_2}$', ha='center', va='center', color='blue')
-    #plt.text(w1u+U/2,w1a-4, r'$\overrightarrow{U}$', ha='center', va='center', color='green')
-    #draw circular arc for alpha_stator between x axis and c1
-    #alpha2 = Arc((0,0),20,10,0,0,alpha_stator,linewidth=1.5, color='red')
-    #plt.gca().add_patch(alpha2)
-   # plt.text(28,3, r'$\alpha_{2}$', ha='center', va='center', color='red')
-    #draw circular arc for beta2 between x axis and w2
-    #beta_stator = Arc((0,0),40,20,0,0,180-beta2,linewidth=1.5, color='blue')
-    #plt.gca().add_patch(beta_stator)
-    #plt.text(30,10, r'$\beta_{2}$', ha='center', va='center', color='blue')
-    #====rotor outlet====
-    #end the line with an arrow
     plt.arrow(0,0,c2u,c2a, head_width=5, head_length=5, fc='red', ec='red', length_includes_head=True)
     plt.arrow(0,0,w2u,w2a, head_width=5, head_length=5, fc='blue', ec='blue', length_includes_head=True)
     plt.arrow(w2u,w2a,U,0, head_width=5, head_length=5, fc='green', ec='green', length_includes_head=True)
-    #add text to the arrows
-    #plt.text(c2u/2-9,c2a/2+4, r'$\overrightarrow{c_3}$', ha='center', va='center', color='red')
-    #plt.text(w2u/2-25,w2a/2+4, r'$\overrightarrow{w_3}$', ha='center', va='center', color='blue')
-   # plt.text(w2u+U/2,w2a-4, r'$\overrightarrow{U}$', ha='center', va='center', color='green')
-    #draw circular arc for alpha_rotor between x axis and c2
-   # alpha3 = Arc((0,0),20,10,0,0,alpha_rotor,linewidth=1.5, color='red')
-   # plt.gca().add_patch(alpha3)
-   # plt.text(8,7, r'$\alpha_{3}$', ha='center', va='center', color='red')
-    #draw circular arc for beta3 between x axis and w3
-   # beta_rotor = Arc((0,0),40,20,0,0,beta2,linewidth=1.5, color='blue')
-    #plt.gca().add_patch(beta_rotor)
-   # plt.text(-13,11, r'$\beta_{3}$', ha='center', va='center', color='blue')
     plt.ylabel(r'Axial velocity [m.s$^{-1}$]')
     plt.xlabel(r'Tangential velocity [m.s$^{-1}$]')
     plt.xlim(-150,300)
@@ -1034,10 +854,10 @@ if __name__=='__main__':
     #uncomment as you wish to draw the plots
     #draw_nozzle_expansion(res["PR_is"],res["Ma_is"],res["p_is"],res["throat_index"],res["PR_act"],res["Ma_act"])
     #draw_expansion_line(res["s"],res["h"],res["s_nozzle_out"],res["h_nozzle_out"],res["s2"],res["h2"],res["h2t"],res["s_out_eta"],res["h_out_eta"],res["p_inlet"],res["p_outlet"],res["p_outlet_total"],res["fluid"],res["z"])
-    draw_velocity_triangles(res["c1u"],res["c1a"],res["w1u"],res["w1a"],res["U"],res["c2u"],res["c2a"],res["w2u"],res["w2a"],res["alpha_stator"],res["beta2"],res["alpha_rotor"])
+    #draw_velocity_triangles(res["c1u"],res["c1a"],res["w1u"],res["w1a"],res["U"],res["c2u"],res["c2a"],res["w2u"],res["w2a"],res["alpha_stator"],res["beta2"],res["alpha_rotor"])
     #save the variables shown in the velocity triangles into a dictionary
     #velocity_triangles = {"c1u":res["c1u"],"c1a":res["c1a"],"w1u":res["w1u"],"w1a":res["w1a"],"U":res["U"],"c2u":res["c2u"],"c2a":res["c2a"],"w2u":res["w2u"],"w2a":res["w2a"],"alpha_stator":res["alpha_stator"],"beta2":res["beta2"],"alpha_rotor":res["alpha_rotor"]}
-    """
+    
     #Genetic Algorithm 
     import multiprocessing
     pool = multiprocessing.Pool(multiprocessing.cpu_count()-2)
@@ -1102,6 +922,6 @@ if __name__=='__main__':
     plot_convergence(log) # Plot the convergence of the GA
     # plot the evolution of different parameters over generations
     #plot_parameters_evolution(log, param_labels,population)
-    """
+    
     toc = timeit.default_timer()
     print(f"Elapsed time: {toc-tic} s")
